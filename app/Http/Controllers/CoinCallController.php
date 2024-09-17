36befbec6e1298dca196d771ce1763f7daad145a
Diony Santos <contato.diony@gmail.com>
2024-09-17 17:22:08 -0300

    refactor: add validator manualy controller

--- app/Http/Controllers/CoinCallController.php
+++ app/Http/Controllers/CoinCallController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CancelOrderRequest;
-use App\Http\Requests\CreateOptionOrderRequest;
-use App\Http\Requests\CreateOrderRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CoinCallController extends Controller
 {
     private function apiRequest($method, $uri, $params = [])
     {
-        $apiKey = 'pGkJp4izHvmjNKXFuTSZVaLcPImHBpDSoeLa8Jmeweo=';
-        $secretKey = 'IOdi32FCjGOxenyk0D76dU3PtB0w/oIfstqpizJLdwQ=';
+        $apiKey = env('COINCALL_API_KEY');
+        $secretKey = env('COINCALL_SECRET_KEY');
         $timestamp = round(microtime(true) * 1000);
         $tsDiff = 5000;
         ksort($params);
@@ -112,54 +111,6 @@ class CoinCallController extends Controller
         return $response;
     }
 
-    public function createOrder(CreateOrderRequest $request)
-    {
-        $validatedData = $request->validated();
-        $tradeType = $validatedData['tradeType'];
-
-        if (in_array($tradeType, [1, 3])) {
-            if (empty($validatedData['qty']) || empty($validatedData['price'])) {
-                return response()->json([
-                    'error' => 'Quantity and price are required for LIMIT and POST_ONLY trade types.'
-                ], 400);
-            }
-        } elseif ($tradeType == 2) {
-            if (empty($validatedData['qty'])) {
-                return response()->json([
-                    'error' => 'Quantity is required for MARKET trade type.'
-                ], 400);
-            }
-        }
-
-        $params = array_filter([
-            'symbol' => $validatedData['symbol'],
-            'clientOrderId' => $validatedData['clientOrderId'] ?? null,
-            'tradeSide' => $validatedData['tradeSide'],
-            'tradeType' => $validatedData['tradeType'],
-            'qty' => $validatedData['qty'] ?? null,
-            'price' => $validatedData['price'] ?? null,
-        ]);
-
-        $uri = '/open/spot/trade/order/v1';
-        $response = $this->apiRequest('POST', $uri, $params);
-
-        return response()->json($response);
-    }
-
-    public function cancelOrder(CancelOrderRequest $request)
-    {
-        $validatedData = $request->validated();
-        $params = array_filter([
-            'clientOrderId' => $validatedData['clientOrderId'] ?? null,
-            'orderId' => $validatedData['orderId'] ?? null,
-        ]);
-
-        $uri = '/open/option/order/cancel/v1';
-        $response = $this->apiRequest('POST', $uri, $params);
-
-        return response()->json($response);
-    }
-
     public function getQueryOrder($id = null)
     {
         if (is_numeric($id)) {
@@ -218,36 +169,6 @@ class CoinCallController extends Controller
         return $response;
     }
 
-    public function createOptionOrder(CreateOptionOrderRequest $request)
-    {
-        $validatedData = $request->validated();
-        $tradeType = $validatedData['tradeType'];
-
-        if (in_array($tradeType, [1])) {
-            if (empty($validatedData['price'])) {
-                return response()->json([
-                    'error' => 'Price required for tradeType 1 LIMIT'
-                ], 400);
-            }
-        };
-
-        $params = array_filter([
-            'clientOrderId' => $validatedData['clientOrderId'] ?? null,
-            'tradeSide' => $validatedData['tradeSide'],
-            'tradeType' => $validatedData['tradeType'],
-            'symbol' => $validatedData['symbol'],
-            'qty' => $validatedData['qty'],
-            'price' => $validatedData['price'] ?? null,
-            'stp' => $validatedData['stp'] ?? null
-
-        ]);
-
-        $uri = '/open/option/order/create/v1';
-        $response = $this->apiRequest('POST', $uri, $params);
-
-        return $response;
-    }
-
     public function getOpenOptionOrders($currency = null, $page = 1, $pageSize = 20)
     {
         $params = [
@@ -294,6 +215,152 @@ class CoinCallController extends Controller
         $response = $this->apiRequest('GET', $uri);
 
         return response()->json($response);
+    }
+
+    public function createOptionOrder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'symbol' => 'required|string',
+            'tradeSide' => 'required|integer|in:1,2',
+            'tradeType' => 'required|integer|in:1,3',
+            'clientOrderId' => 'nullable|numeric',
+            'qty' => 'required|numeric',
+            'price' => 'nullable|numeric',
+            'stp' => 'nullable|in:1,2,3',
+        ], $this->getMessagesCreateOptionOrder());
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $validatedData = $validator->validated();
+        $tradeType = $validatedData['tradeType'];
+
+        if (in_array($tradeType, [1])) {
+            if (empty($validatedData['price'])) {
+                return response()->json([
+                    'error' => 'price é obrigatório para o tipo de negociação LIMIT'
+                ], 400);
+            }
+        };
+
+        $params = array_filter([
+            'clientOrderId' => $validatedData['clientOrderId'] ?? null,
+            'tradeSide' => $validatedData['tradeSide'],
+            'tradeType' => $validatedData['tradeType'],
+            'symbol' => $validatedData['symbol'],
+            'qty' => $validatedData['qty'],
+            'price' => $validatedData['price'] ?? null,
+            'stp' => $validatedData['stp'] ?? null
+
+        ]);
+
+        $uri = '/open/option/order/create/v1';
+        $response = $this->apiRequest('POST', $uri, $params);
+
+        return $response;
+    }
+
+    public function createOrder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'symbol' => 'required|string',
+            'tradeSide' => 'required|integer|in:1,2',
+            'tradeType' => 'required|integer|in:1,2,3',
+            'clientOrderId' => 'nullable|numeric',
+            'qty' => 'nullable|numeric',
+            'price' => 'nullable|numeric',
+        ], $this->getMessagesCreateOrder());
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $validatedData = $validator->validated();
+        $tradeType = $validatedData['tradeType'];
+
+        if (in_array($tradeType, [1, 3])) {
+            if (empty($validatedData['qty']) || empty($validatedData['price'])) {
+                return response()->json([
+                    'error' => 'qty e price são obrigatórios para os tipos de negociação LIMIT e POST_ONLY.'
+                ], 400);
+            }
+        } elseif ($tradeType == 2) {
+            if (empty($validatedData['qty'])) {
+                return response()->json([
+                    'error' => 'qty é obrigatório para o tipo de negociação MARKET.'
+                ], 400);
+            }
+        }
+
+        $params = array_filter([
+            'symbol' => $validatedData['symbol'],
+            'clientOrderId' => $validatedData['clientOrderId'] ?? null,
+            'tradeSide' => $validatedData['tradeSide'],
+            'tradeType' => $validatedData['tradeType'],
+            'qty' => $validatedData['qty'] ?? null,
+            'price' => $validatedData['price'] ?? null,
+        ]);
+
+        $uri = '/open/spot/trade/order/v1';
+        $response = $this->apiRequest('POST', $uri, $params);
+
+        return response()->json($response);
+    }
+
+    public function cancelOrder(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'clientOrderId' => 'nullable|string|required_without:orderId',
+            'orderId' => 'nullable|string|required_without:clientOrderId',
+        ], $this->getMessagesCancelOrder());
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $params = array_filter([
+            'clientOrderId' => $validatedData['clientOrderId'] ?? null,
+            'orderId' => $validatedData['orderId'] ?? null,
+        ]);
+
+        $uri = '/open/option/order/cancel/v1';
+        $response = $this->apiRequest('POST', $uri, $params);
+
+        return response()->json($response);
+    }
+
+    protected function getMessagesCancelOrder()
+    {
+        return [
+            'clientOrderId.required_without' => 'O campo clientOrderId é obrigatório quando orderId não está presente.',
+            'orderId.required_without' => 'O campo orderId é obrigatório quando clientOrderId não está presente.',
+        ];
+    }
+
+    protected function getMessagesCreateOptionOrder()
+    {
+        return [
+            'tradeSide.in' => 'O campo tradeSide deve ter um dos seguintes valores: 1 ou 2.',
+            'tradeType.in' => 'O campo tradeType deve ter um dos seguintes valores: 1 ou 3.',
+            'stp.in' => 'O campo stp deve ter um dos seguintes valores: 1 (CM - Cancel Maker), 2 (CT - Cancel Taker), ou 3 (CB - Cancel Both).',
+        ];
+    }
+
+    protected function getMessagesCreateOrder()
+    {
+        return [
+            'tradeSide.in' => 'O campo tradeSide deve ter um dos seguintes valores: 1 ou 2.',
+            'tradeType.in' => 'O campo tradeType deve ter um dos seguintes valores: 1 ou 3.',
+        ];
     }
 }
 
