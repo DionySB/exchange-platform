0c0f3136fe53733ef6b299e52558f11a0ec67919
Diony Santos <contato.diony@gmail.com>
2024-11-12 18:45:12 -0300

    FEAT: new Binance Api

---
+++ app/Http/Controllers/BinanceController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BinanceController extends Controller
+{
+
+    private function apiRequest($method, $uri, $params = [])
+    {
+        $apiKey = env('BINANCE_API_KEY_TEST');
+        $secretKey = env('BINANCE_SECRET_KEY_TEST');
+        $timestamp = round(microtime(true) * 1000);
+        $recvWindow = 5000;
+
+        $params['timestamp'] = $timestamp;
+        $params['recvWindow'] = $recvWindow;
+        $params = array_filter($params, fn($value) => !is_null($value)); // Remove null values
+        $queryString = http_build_query($params);
+        $signature = hash_hmac('sha256', $queryString, $secretKey);
+        $url = "https://testnet.binance.vision{$uri}?signature={$signature}";
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 2,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_HTTPHEADER => [
+                "X-MBX-APIKEY: {$apiKey}",
+                "Content-Type: application/x-www-form-urlencoded"
+            ],
+            CURLOPT_SSL_VERIFYPEER => false,
+        ]);
+
+        // Envia os parâmetros de acordo com o método
+        if ($method === 'GET') {
+            curl_setopt($curl, CURLOPT_URL, $url . "&" . $queryString);
+        } else {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+
+        $response = curl_exec($curl);
+        dd([
+            'response' => $response,
+            'url' => $url,
+            'signature' => $signature,
+            'queryString' => $queryString,
+            'timestamp' => $timestamp,
+        ]);
+        curl_close($curl);
+
+        return json_decode($response, true);
+    }
+}
