165549042abf8aa9961fb2e7c72ddcafc198fd85
Diony Santos <contato.diony@gmail.com>
2024-11-14 17:39:07 -0300

    FEAT: newOrder endpoint

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -7,8 +7,8 @@ class BinanceController extends Controller
 
     private function apiRequest($method, $uri, $params = [])
     {
-        $apiKey = env('BINANCE_API_KEY_TEST');
-        $secretKey = env('BINANCE_SECRET_KEY_TEST');
+        $apiKey = env('BINANCE_API_KEY');
+        $secretKey = env('BINANCE_SECRET_KEY');
         $timestamp = round(microtime(true) * 1000);
         $recvWindow = 5000;
 
@@ -17,7 +17,7 @@ class BinanceController extends Controller
         $params = array_filter($params, fn($value) => !is_null($value));
         $queryString = http_build_query($params);
         $signature = hash_hmac('sha256', $queryString, $secretKey);
-        $url = "https://testnet.binance.vision{$uri}?signature={$signature}";
+        $url = "https://api.binance.com{$uri}?signature={$signature}";
 
         $curl = curl_init();
         curl_setopt_array($curl, [
@@ -41,14 +41,6 @@ class BinanceController extends Controller
         $response = curl_exec($curl);
         curl_close($curl);
 
-        dd([
-            'response' => $response,
-            'url' => $url,
-            'signature' => $signature,
-            'queryString' => $queryString,
-            'timestamp' => $timestamp,
-        ]);
-
         return json_decode($response, true);
     }
 
@@ -57,6 +49,76 @@ class BinanceController extends Controller
         $uri = '/api/v3/account';
 
         $response = $this->apiRequest('GET', $uri);
+
+        return $response;
+    }
+
+    public function newOrder( array $dados)
+    {
+        $uri = '/api/v3/order';
+        if(empty($dados['symbol']) && empty($dados['side']) && empty($dados['type'])){
+            return [
+                'success' => false,
+                'message' => 'Os campos symbol, side e type sao obrigatorios.'
+            ];
+        }
+
+        if ($dados['type'] === 'LIMIT') {
+            if (empty($dados['timeInForce']) || empty($dados['quantity']) || empty($dados['price'])) {
+                return [
+                    'success' => false,
+                    'message' => 'Para ordens LIMIT, os campos timeInForce, quantity e price são obrigatórios.'
+                ];
+            }
+        } elseif ($dados['type'] === 'MARKET') {
+            if (empty($dados['quantity']) && empty($dados['quoteOrderQty'])) {
+                return [
+                    'success' => false,
+                    'message' => 'MARKET, requer quantity ou quoteOrderQty.'
+                ];
+            }
+        } elseif ($dados['type'] === 'STOP_LOSS' || $dados['type'] === 'TAKE_PROFIT') {
+            if (empty($dados['quantity']) || (empty($dados['stopPrice']) && empty($dados['trailingDelta']))) {
+                return [
+                    'success' => false,
+                    'message' => 'STOP_LOSS e TAKE_PROFIT, requer quantity e stopPrice ou trailingDelta.'
+                ];
+            }
+        } elseif ($dados['type'] === 'STOP_LOSS_LIMIT' || $dados['type'] === 'TAKE_PROFIT_LIMIT') {
+            if (empty($dados['timeInForce']) || empty($dados['quantity']) || empty($dados['price']) || (empty($dados['stopPrice']) && empty($dados['trailingDelta']))) {
+                return [
+                    'success' => false,
+                    'message' => 'STOP_LOSS_LIMIT e TAKE_PROFIT_LIMIT requer timeInForce, quantity, price e stopPrice ou trailingDelta.'
+                ];
+            }
+        } elseif ($dados['type'] === 'LIMIT_MAKER') {
+            if (empty($dados['quantity']) || empty($dados['price'])) {
+                return [
+                    'success' => false,
+                    'message' => 'LIMIT_MAKER, requer quantity e price.'
+                ];
+            }
+        }
+
+        $params = [
+            'symbol' => $dados['symbol'],
+            'side' => $dados['side'],
+            'type' => $dados['type'],
+            'timeInForce' => $dados['timeInForce'] ?? null,
+            'quantity' => $dados['quantity'] ?? null,
+            'quoteOrderQty' => $dados['quoteOrderQty'] ?? null,
+            'price' => $dados['price'] ?? null,
+            'newClientOrderId' => $dados['newClientOrderId'] ?? null,
+            'strategyId' => $dados['strategyId'] ?? null,
+            'strategyType' => $dados['strategyType'] ?? null,
+            'stopPrice' => $dados['stopPrice'] ?? null,
+            'trailingDelta' => $dados['trailingDelta'] ?? null,
+            'icebergQty' => $dados['icebergQty'] ?? null,
+            'newOrderRespType' => $dados['newOrderRespType'] ?? 'FULL',
+            'selfTradePreventionMode' => $dados['selfTradePreventionMode'] ?? null,
+        ];
+
+        $response = $this->apiRequest('POST', $uri, $params);
 
         return $response;
     }
